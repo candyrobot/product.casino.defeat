44ed492598ff16fb8d6a85c7d0c97e1e23438c0e
Hideaki Tanaka <findwkwk@gmail.com>
2022-04-16 18:21:54 +0900

    update

--- src/strategy.Blackjack.js
+++ src/strategy.Blackjack.js
@@ -1,76 +1,50 @@
 class StrategyBlackjack {
 	constructor() {
-		this.strategyPair = [ // 10,A,2,3,4,5,6,7,8,9  // x: 2,3,4,5,6,7,8,9,10,A
-			[],
-			['P','P','P','P','P','P','P','P','P','H'], // A:A
-			['H','P','P','P','P','P','H','H','H','H'], // 2:2
-			['H','H','P','P','P','P','H','H','H','H'], // 3:3
-			['H','H','H','H','H','H','H','H','H','H'], // 4:4
-			[], // 5:5
-			[], // 6:6
-			[], // 7:7
-			[], // 8:8
-			[], // 9:9
-			['S','S','S','S','S','S','S','S','S','S'], // 10:10
-		]
-		this.strategySoft = [
-			[], // ~S17
-			[], // S18
-			[], // S19~
-		]
-		this.strategy = [
-			[], // ~8
-			[], // 9
-			[], // 10
-			[], // 11
-			[], // 12
-			[], // 13
-			[], // 14
-			[], // 15
-			[], // 16
-			[], // 17~
-		]
+		this.strategyPair = {
+			4: { 2:'H',3:'P',4:'P',5:'P',6:'P',7:'P',8:'H',9:'H',10:'H',1:'H' }, // 2:2
+			6: { 2:'H',3:'H',4:'P',5:'P',6:'P',7:'P',8:'H',9:'H',10:'H',1:'H' }, // 3:3
+			8: { 2:'H',3:'H',4:'H',5:'H',6:'H',7:'H',8:'H',9:'H',10:'H',1:'H' }, // 4:4
+			10: { 2:'D',3:'D',4:'D',5:'D',6:'D',7:'D',8:'D',9:'D',10:'H',1:'H' }, // 5:5
+			12: { 2:'P',3:'P',4:'P',5:'P',6:'P',7:'H',8:'H',9:'H',10:'H',1:'H' }, // 6:6
+			14: { 2:'P',3:'P',4:'P',5:'P',6:'P',7:'P',8:'H',9:'H',10:'S',1:'H' }, // 7:7
+			16: { 2:'P',3:'P',4:'P',5:'P',6:'P',7:'P',8:'P',9:'P',10:'H',1:'H' }, // 8:8
+			18: { 2:'P',3:'P',4:'P',5:'P',6:'P',7:'S',8:'P',9:'P',10:'S',1:'S' }, // 9:9
+			20: { 2:'S',3:'S',4:'S',5:'S',6:'S',7:'S',8:'S',9:'S',10:'S',1:'S' }, // 10:10
+			2: { 2:'P',3:'P',4:'P',5:'P',6:'P',7:'P',8:'P',9:'P',10:'P',1:'H' }, // A:A
+		}
+		this.strategySoft = {
+			17: { 2:'H',3:'H',4:'H',5:'H',6:'H',7:'H',8:'H',9:'H',10:'H',1:'H' }, // ~S17
+			18: { 2:'S',3:'S',4:'S',5:'S',6:'S',7:'S',8:'S',9:'H',10:'H',1:'S' }, // S18
+			19: { 2:'S',3:'S',4:'S',5:'S',6:'S',7:'S',8:'S',9:'S',10:'S',1:'S' }, // S19~
+		}
+		this.strategy = {
+			8: { 2:'H',3:'H',4:'H',5:'H',6:'H',7:'H',8:'H',9:'H',10:'H',1:'H' },
+			9: { 2:'D',3:'D',4:'D',5:'D',6:'D',7:'H',8:'H',9:'H',10:'H',1:'H' },
+			10: { 2:'D',3:'D',4:'D',5:'D',6:'D',7:'D',8:'D',9:'D',10:'H',1:'H' },
+			11: { 2:'D',3:'D',4:'D',5:'D',6:'D',7:'D',8:'D',9:'D',10:'H',1:'H' },
+			12: { 2:'H',3:'H',4:'S',5:'S',6:'S',7:'H',8:'H',9:'H',10:'H',1:'H' },
+			13: { 2:'S',3:'S',4:'S',5:'S',6:'S',7:'H',8:'H',9:'H',10:'H',1:'H' },
+			14: { 2:'S',3:'S',4:'S',5:'S',6:'S',7:'H',8:'H',9:'H',10:'H',1:'H' },
+			15: { 2:'S',3:'S',4:'S',5:'S',6:'S',7:'H',8:'H',9:'H',10:'H',1:'H' },
+			16: { 2:'S',3:'S',4:'S',5:'S',6:'S',7:'H',8:'H',9:'H',10:'H',1:'H' },
+			17: { 2:'H',3:'H',4:'H',5:'H',6:'H',7:'H',8:'H',9:'H',10:'H',1:'H' },
+		}
 	}
 	getAction(playerCards, dealerCard) {
 		let playerNumber = playerCards.reduce((p, v) => p + v, 0)
-		if (this.checkCards(playerCards) === 'Pair') {
-			return this.strategyPair[playerCards[0]][dealerCard]
-		} else if (this.checkCards(playerCards) === 'IncludedA') {
-			switch (true) {
-				case playerNumber <== 17:
-					return this.strategyPair[0][dealerCard]
-				case playerNumber === 18:
-					return this.strategyPair[1][dealerCard]
-				case playerNumber >== 19:
-					return this.strategyPair[2][dealerCard]
-			}
-		} else {
-			switch (true) {
-				case playerNumber <== 8:
-					return this.strategyPair[0][dealerCard]
-				case playerNumber === 9:
-					return this.strategyPair[1][dealerCard]
-				case playerNumber === 10:
-					return this.strategyPair[2][dealerCard]
-				case playerNumber === 11:
-					return this.strategyPair[3][dealerCard]
-				case playerNumber === 12:
-					return this.strategyPair[4][dealerCard]
-				case playerNumber === 13:
-					return this.strategyPair[5][dealerCard]
-				case playerNumber === 14:
-					return this.strategyPair[6][dealerCard]
-				case playerNumber === 15:
-					return this.strategyPair[7][dealerCard]
-				case playerNumber === 16:
-					return this.strategyPair[8][dealerCard]
-				case playerNumber >== 17:
-					return this.strategyPair[9][dealerCard]
-			}
-		}
+		if (this.checkCards(playerCards) === 'Pair' && playerCards[0] === 2)
+			let d = { 2: 'S', 3: 'H' }
+			d[dealerCard]
+
+
 	}
 	// @return string 'Pair' 'IncludedA'
 	checkCards(playerCards) {
-		// return 
+		if (playerCards.length === 2 && playerCards[0] === playerCards[1])
+			return 'Pair'
 	}
+}
+
+dependsOnA() {
+	return 'D'
 }
