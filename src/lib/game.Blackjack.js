0df9df9c64574e5006994cfa1c39d1c0134f91b6
Hideaki Tanaka <findwkwk@gmail.com>
2022-04-20 04:11:08 +0900

    update

--- src/lib/game.Blackjack.js
+++ src/lib/game.Blackjack.js
@@ -1,9 +1,7 @@
 import PlayingCards from './PlayingCards'
 import StrategyBlackjack from './strategy.Blackjack'
 
-// INFO: blackjackストラテジープログラム
-
-// INFO: 1shoeだけ担当するdealer
+// INFO: 1shoeだけ担当する。
 class Blackjack {
 	constructor() {
 		this.playingCards = new PlayingCards(8).shuffle().get()
@@ -12,7 +10,7 @@ class Blackjack {
 	}
 	/**
 	 * @param {number} betValue
-	 * @return {number} income
+	 * @return {number | undefined} income - undefined: end of shoe
 	 */
 	play(n) {
 		let { playerCards, dealerCard } = this._dealInit()
@@ -23,26 +21,65 @@ class Blackjack {
 	 * @param {Array} playerHands
 	 * @param {number} dealerCard
 	 * @param {number} i - playerHands{Array} の index
+	 * @return {number} income
 	 */
 	_playHand(playerHands, dealerCard, i) {
 		let action = this.strategyBlackjack.getAction(playerHands[i], dealerCard)
-		switch(action) {
+		switch (action) {
 			case 'H':
 				playerHands[i].push(this._dealCard())
-				this._playHand(playerHands, dealerCard, i)
-				break;
+				if (this._isBust(playerHands[i]))
+					'Lose'
+					if (playerHands[i+1] === undefined)
+						return this._collectIncome()
+					else
+						// next hand.
+						this._playHand(playerHands, dealerCard, i+1)
+				else
+					this._playHand(playerHands, dealerCard, i)
+				break
 			case 'S':
-				// next hand or end.
+				if (playerHands[i+1] === undefined)
+					for (var i = 0; i < playerHands.length; i++) {
+						let result = this._battleToDealer(playerHands[i], dealerCard)
+					}
+				else
+					// next hand.
+					this._playHand(playerHands, dealerCard, i+1)
 				break
 			case 'P':
-				playerHands[i+1].push(playerHands[i].shift())
+				let splitedIndex = playerHands.length
+				playerHands[splitedIndex].push(playerHands[i].shift())
+				
 				playerHands[i].push(this._dealCard())
-				playerHands[i+1].push(this._dealCard())
-				for (var i = 0; i < playerHands.length; i++) {
-					this._playHand(playerHands[i], dealerCard)
-				}
+				this._playHand(playerHands, dealerCard, i)
+
+				playerHands[splitedIndex].push(this._dealCard())
+				this._playHand(playerHands, dealerCard, splitedIndex)
 				break
 		}
+	}
+	/** TODO: 1を11とする処理
+	 * @return {string} - 'Win' 'Lose' 'Push' 'BlackJack'
+	 * // TODO: ↓
+	 * @return {object} - { result: 'Win' 'Lose' 'Push', isBlackjack: boolean }
+	 */
+	_battleToDealer(handCards, dealerCard) {
+		let dealerCards = [dealerCard, this._dealCard()]
+		if (dealerCards[0] === 10 && dealerCards[1] === 1)
+			// TODO: ある条件下でpushなのかloseなのか調べる
+			return 'BlackJack'
+		while (dealerCards.reduce((p, v) => p + v, 0) <= 16) {
+			dealerCards.push(this._dealCard())
+		}
+		if (this._isBust(dealerCards))
+			return 'Win'
+		else if (dealerCards.reduce((p, v) => p + v, 0) === handCards.reduce((p, v) => p + v, 0))
+			return 'Push'
+		else if (dealerCards.reduce((p, v) => p + v, 0) > handCards.reduce((p, v) => p + v, 0))
+			return 'Lose'
+		else
+			return 'Win'
 	}
 	/** INFO: プレイヤーに2枚, ディーラー1枚
 	 * @return { playerCards: Array, dealerCard: number }
@@ -56,6 +93,10 @@ class Blackjack {
 		this.usedCards.push(n)
 		return n
 	}
+	/**
+	 * @return {boolean}
+	 */
+	_isBust(handCards) {}
 	_isBlackjack() {}
 	// x: @param string 'S', 'H', 'P'(split), 'D'
 	// setAction(action) {
