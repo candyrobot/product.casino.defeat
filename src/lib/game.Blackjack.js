9f8bd7495b551588bd0d758a09a9a215b9acb791
Hideaki Tanaka <findwkwk@gmail.com>
2022-04-21 17:16:10 +0900

    update

--- src/lib/game.Blackjack.js
+++ src/lib/game.Blackjack.js
@@ -10,62 +10,75 @@ class Blackjack {
 	}
 	/**
 	 * @param {number} betValue
-	 * @return {number | undefined} income - undefined: end of shoe
+	 * @return {object | undefined} - undefined: end of shoe
+	 *   { income: number, results: Array ['Win' 'Lose' 'Blackjack' 'Push'] }
 	 */
 	play(n) {
-		let { playerCards, dealerCard } = this._dealInit()
+		let { playerCards, dealerCards } = this._dealInit()
+		let isInsurance = false
+		if (dealerCards[0] === 1) {
+			isInsurance = this._isInsurance()
+			if (dealerCards[1] === 10)
+				return isInsurance ?
+					{ results: ['Lose'], income: 0 } :
+					{ results: ['Lose'], income: -n }
+			else
+				if (isInsurance)
+					console.log('TODO: 総incomeからinsurance分をマイナスする -=n/2')
+		}
+		if (playerCards[0] === 1 && playerCards[1] === 10 || playerCards[0] === 10 && playerCards[1] === 1)
+			return { results: ['Blackjack'], income: n * 1.5 }
 		let playerHands = [playerCards]
-		this._playHand(playerHands, dealerCard, 0)
+		this._playHand(playerHands, dealerCards, 0)
 	}
 	/** INFO: ひとつのハンド分
 	 * @param {Array} playerHands
-	 * @param {number} dealerCard
+	 * @param {Array} dealerCards
 	 * @param {number} i - playerHands{Array} の index
-	 * @return {number} income
+	 * @return {Array} results ['Win' 'Lose' 'Blackjack' 'Push']
 	 */
-	_playHand(playerHands, dealerCard, i) {
-		let action = this.strategyBlackjack.getAction(playerHands[i], dealerCard)
+	_playHand(playerHands, dealerCards, i) {
+		let action = this.strategyBlackjack.getAction(playerHands[i], dealerCards[0])
 		switch (action) {
 			case 'H':
 				playerHands[i].push(this._dealCard())
 				if (this._isBust(playerHands[i]))
 					'Lose'
-					if (playerHands[i+1] === undefined)
-						return this._collectIncome()
-					else
-						// next hand.
-						this._playHand(playerHands, dealerCard, i+1)
+					this._nextOrEnd(playerHands, dealerCards, i)
 				else
-					this._playHand(playerHands, dealerCard, i)
+					this._playHand(playerHands, dealerCards, i)
 				break
 			case 'S':
-				if (playerHands[i+1] === undefined)
-					for (var i = 0; i < playerHands.length; i++) {
-						let result = this._battleToDealer(playerHands[i], dealerCard)
-					}
-				else
-					// next hand.
-					this._playHand(playerHands, dealerCard, i+1)
+				this._nextOrEnd(playerHands, dealerCards, i)
 				break
 			case 'P':
 				let splitedIndex = playerHands.length
 				playerHands[splitedIndex].push(playerHands[i].shift())
 				
 				playerHands[i].push(this._dealCard())
-				this._playHand(playerHands, dealerCard, i)
+				this._playHand(playerHands, dealerCards, i)
 
 				playerHands[splitedIndex].push(this._dealCard())
-				this._playHand(playerHands, dealerCard, splitedIndex)
+				this._playHand(playerHands, dealerCards, splitedIndex)
 				break
 		}
+	}
+	_nextOrEnd(playerHands, dealerCards, i) {
+		if (playerHands[i+1] === undefined)
+			return playerHands.reduce((p, handCards) => {
+				let result = this._battleToDealer(handCards, dealerCards)
+				return p + this._collectIncome(result)
+			}, 0)
+		else
+			// next hand.
+			this._playHand(playerHands, dealerCards, i+1)
 	}
 	/** TODO: 1を11とする処理
 	 * @return {string} - 'Win' 'Lose' 'Push' 'BlackJack'
 	 * // TODO: ↓
 	 * @return {object} - { result: 'Win' 'Lose' 'Push', isBlackjack: boolean }
 	 */
-	_battleToDealer(handCards, dealerCard) {
-		let dealerCards = [dealerCard, this._dealCard()]
+	_battleToDealer(handCards, dealerCards) {
 		if (dealerCards[0] === 10 && dealerCards[1] === 1)
 			// TODO: ある条件下でpushなのかloseなのか調べる
 			return 'BlackJack'
@@ -82,7 +95,7 @@ class Blackjack {
 			return 'Win'
 	}
 	/** INFO: プレイヤーに2枚, ディーラー1枚
-	 * @return { playerCards: Array, dealerCard: number }
+	 * @return { playerCards: Array, dealerCards: Array }
 	 */
 	_dealInit() {
 
@@ -98,6 +111,7 @@ class Blackjack {
 	 */
 	_isBust(handCards) {}
 	_isBlackjack() {}
+	_isInsurance() { return false }
 	// x: @param string 'S', 'H', 'P'(split), 'D'
 	// setAction(action) {
 
